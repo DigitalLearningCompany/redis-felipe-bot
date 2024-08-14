843d94218f5b6de7ea0ad902e8f0e68072ec4f87
DigitalLearningCompany <162745763+DigitalLearningCompany@users.noreply.github.com>
2024-08-14 13:24:57 -0300

    adiciona ponto de exclamação nas mensagens

--- introMessage.js
+++ introMessage.js
@@ -7,7 +7,7 @@ const introMessage = (name) => fmt`
 
 2️⃣✅ Eu ${bold('NÃO VOU')} alavancar o seu dinheiro dentro da sua conta se a sua conta não estiver aquecida. Enquanto sua vez não chega na fila de espera, ${bold('aqueça a sua conta usando os sinais do aplicativo Games Lucrativos e vá lucrando e usando a sua conta!')}
 
-3️⃣✅ O lucro é ${bold('GARANTIDO')} pois se eu entrar na sua conta e não conseguir alavancar (isso é muito raro de acontecer) eu vou devolver a sua conta com o valor de saldo inicial que tinha lá dobrado. 
+3️⃣✅ O lucro é ${bold('GARANTIDO')} pois se eu entrar na sua conta e não conseguir alavancar (isso é muito raro de acontecer) eu vou devolver a sua conta com o valor de saldo inicial que tinha lá dobrado!
 
 ⬇️ ${bold('CLIQUE NO BOTÃO ABAIXO')} para entrar no meu grupo VIP e pegar seu: ${bold('Acesso ao aplicativo Games Lucrativos e conseguir assistir a aula onde eu te ensino a usar o app e aquecer a sua conta!')} ⬇️
 `;
--- secondMessage.js
+++ secondMessage.js
@@ -3,7 +3,7 @@ const { bold, fmt } = require("@grammyjs/parse-mode");
 const secondMessage = (name, queuePosition) => fmt`
 📲 ${name}, ${bold('sua posição na fila de espera é')}: ${queuePosition} 
 
-🚨 ${bold('Atenção')}: Continue aquecendo a sua conta com o aplicativo Games Lucrativos e sendo um jogador ativo. ${bold('Quanto mais aquecida estiver a sua conta, maior será a alavancagem.')} 
+🚨 ${bold('Atenção')}: Continue aquecendo a sua conta com o aplicativo Games Lucrativos e sendo um jogador ativo. ${bold('Quanto mais aquecida estiver a sua conta, maior será a alavancagem!')} 
 
 🏆 ${bold('Hack de aquecimento')}: No app clique em “Prêmios” e jogue com o foco de conquistar os prêmios. Assim você irá lucrar mais e a sua conta atingirá o nível máximo de aquecimento para a ${bold('sua alavancagem particular ter o valor multiplicado em até 30X!')}
 
